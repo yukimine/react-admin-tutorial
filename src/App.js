4e1a5e01d1fa88154ed12f9bfb2a935bc0da436f
yukimine <yukimine_hayashi@voyagegroup.com>
2021-07-17 01:51:33 +0900

    handle relationships

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import * as React from "react"
 import { Admin, Resource } from 'react-admin';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
+import { PostList } from "./posts";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
