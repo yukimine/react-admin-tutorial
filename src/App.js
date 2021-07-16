b201ee76f814313dfb831f4aa87b8fb4829f83e2
yukimine <yukimine_hayashi@voyagegroup.com>
2021-07-17 02:07:24 +0900

    edit and create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import * as React from "react"
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, EditGuesser } from 'react-admin';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
-import { PostList } from "./posts";
+import { PostEdit, PostList, PostCreate } from "./posts";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
