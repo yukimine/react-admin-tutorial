b38181831bcba1625b077ccdb80c9fb9db6e6d91
yukimine <yukimine_hayashi@voyagegroup.com>
2021-07-17 23:46:37 +0900

    add login page

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import { PostEdit, PostList, PostCreate } from "./posts";
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import Dashboard from './Dashboard';
+import authProvider from './authProvider'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
