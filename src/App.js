40fbfe6380db3ca755750b6ce4f2c6d47bdb4137
yukimine <yukimine_hayashi@voyagegroup.com>
2021-07-17 23:31:46 +0900

    customize icons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Admin, Resource, EditGuesser } from 'react-admin';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
 import { PostEdit, PostList, PostCreate } from "./posts";
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-    <Resource name="users" list={UserList} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
